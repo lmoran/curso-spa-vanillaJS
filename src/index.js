0887b5d9588445bb969d277d44505da0052cb1b7
Lmoran <luismoran0293@gmail.com>
2020-04-16 16:48:04 -0500

    Creamos el template de Home dentro de la carp page

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('Hello, Si funcionó')
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+//generamos una funcion que nos va retornar la vista que necesitamos:
+const Home = () => {
+  const view = `
+    <div class="Characters">
+      <article class="Character-item">
+        <a href= "#/1/">
+        <img src= "image" alt="name">
+        <h2>Name</h2>
+        </a>
+      </article>
+    </div>
+`
+  return view
+}
+//exportamos el archivo para que lo podamos utilizar en otros archivos de JavaScript
+export default Home
