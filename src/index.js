78cdeab6dd7cd76bd978aca7a384096832b61dcf
Lmoran <luismoran0293@gmail.com>
2020-04-17 10:41:31 -0500

    Indexando el template header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('Hello, Si funcionó')
+import router from './routes/index'
+//creamos el evento que va estar escuchando que la carga de la pagina ha sucedido, como va tarer todos los elementos en una sola carga:
+window.addEventListener('load', router)
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,18 +5,18 @@ import Character from '../pages/Character'
 import Error404 from '../pages/Error404'
 
 //vamos a establecer las rutas:
-const router = {
+const routes = {
   '/': Home,
   '/:id': Character,
   '/contact': 'Contact'
 }
 //vamos a crear nuestro manejador routes se va encargar de mostrar los elementos segun la lógica, obtener valores del navegador y como vamos a estructurarlos.
-const routes = async () => {
+const router = async () => {
   //vamos a establecer los templates que los vamos a indexar a public index.html osea los templates los vamos a renderizar en las clases de header y content de index.html de public.
   //creamos una constante y le vamos a psar null si es caso que no la encontremos o el id del elemento:
-  const header = null || document.getElementById('Header')
-  const content = null || document.getElementById('Content')
+  const header = null || document.getElementById('header')
+  const content = null || document.getElementById('content')
   //las indexamos: ojo le colocamos await porque vamos a esperar la petición de ese template Header
-  header.innerHTML = await Header
+  header.innerHTML = await Header()
 }
 export default router
